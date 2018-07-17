0d258e54e2ae6f6215b8b0c5469057e66fd7643a
10305-6 <a10305-6@iMac-6.local>
2018-07-17 14:27:57 +0700

    ครั้งแรก

---
+++ app/src/main/java/com/example/a10305_6/myapplab1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a10305_6.myapplab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //ประกาศตัวแปล
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}//เมนหลัก
